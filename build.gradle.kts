bc452e0f36d17bf4667f31005ef995a781fc92f8
Jordan L. Araujo Jr. <jordanjr.92@gmail.com>
2025-09-03 08:48:55 -0600

    Unify JVM versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import com.android.build.gradle.BaseExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -10,9 +11,9 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
 }
 
-// Add JVM setup on all gradle subprojects from Gradle Version Catalog
+/** Align JVM target versions between Java and Kotlin compilation tasks. */
 val jvm = JvmTarget.fromTarget(libs.versions.jvmVersion.get())
-allprojects {
+subprojects {
     tasks.withType(KotlinCompile::class.java).configureEach {
         compilerOptions.jvmTarget = jvm
     }
@@ -20,12 +21,29 @@ allprojects {
         sourceCompatibility = jvm.target
         targetCompatibility = jvm.target
     }
-    if (pluginManager.hasPlugin("com.android.base")) {
-        pluginManager.withPlugin("com.android.base") {
-            extensions.configure("android", Action<BaseExtension> {
-                compileOptions.sourceCompatibility = JavaVersion.toVersion(jvm.target)
-                compileOptions.targetCompatibility = JavaVersion.toVersion(jvm.target)
-            })
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.base")) {
+            pluginManager.withPlugin("com.android.base") {
+                extensions.configure("android", Action<BaseExtension> {
+                    compileOptions.sourceCompatibility = JavaVersion.toVersion(jvm.target)
+                    compileOptions.targetCompatibility = JavaVersion.toVersion(jvm.target)
+                })
+            }
+        }
+        // Apply JVM toolchain to Kotlin projects
+        if (plugins.hasPlugin("org.jetbrains.kotlin.jvm") || plugins.hasPlugin("org.jetbrains.kotlin.android")) {
+            extensions.findByType(KotlinProjectExtension::class.java)?.apply {
+                    jvmToolchain(jvm.target.toInt())
+                }
+        }
+
+        // Apply JVM toolchain to Java projects
+        if (plugins.hasPlugin("java")) {
+            extensions.findByType(JavaPluginExtension::class.java)?.apply {
+                toolchain.languageVersion.set(JavaLanguageVersion.of(jvm.target.toInt()))
+                sourceCompatibility = JavaVersion.toVersion(jvm.target)
+                targetCompatibility = JavaVersion.toVersion(jvm.target)
+            }
         }
     }
 }
